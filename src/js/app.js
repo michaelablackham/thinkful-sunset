16f639e3f2aeff8f186a8e00c27b69c2879b8186
Michaela Blackham <Michaelablackham@gmail.com>
2017-07-10 19:47:23 -0600

    setting up environment

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+$(function() {
+  'use strict';
+
+  var BASE_URL = 'https://sunsetwx.com/'
+  var REG_KEY = '0OdKkau61SPKMU0GcLfPz9cI';
+  var ENPOINT = '/v1/register';
+  var PASSWORD = "B13b18a2"
+
+  console.log("hello world!")
+
+}
+});
