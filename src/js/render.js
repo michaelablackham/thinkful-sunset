12e02b0b31f4ac71d3d3b126e9865dc543a1d958
Michaela Blackham <Michaelablackham@gmail.com>
2017-07-24 21:43:29 -0600

    changing layout

---
+++ src/js/render.js
@@ -0,0 +1,47 @@
+var App = App || {};
+
+App.Render = (function($) {
+  'use strict';
+
+  var SECTION_ELEMENTS = {};
+
+  // set the initial currentPage
+  function setCurrentPage(newCurrentPage) {
+    App.State.set({currentPage: newCurrentPage});
+  }
+
+  function renderSunevent() {
+    // Renders the app
+    var state = App.State.get();
+
+    // default to hiding all current page, then show the currentpage
+    Object.keys(SECTION_ELEMENTS).forEach(function (currentPage) {
+      SECTION_ELEMENTS[currentPage].hide();
+    });
+
+    // SHOW THE CURRENT PAGE
+    SECTION_ELEMENTS[state.currentPage].show();
+
+    //SWITCH TO SHOW THE NEW PAGE and run that pages' function
+    switch (state.currentPage) {
+      case 'pageHome':
+        App.Start.renderStartPage(state, SECTION_ELEMENTS[state.currentPage]);
+        break;
+      default:
+        throw new Error('Unexpected page.');
+    }
+  }
+
+  $(function () {
+    Object.assign(SECTION_ELEMENTS, {
+      pageHome: $('#page-home')
+
+    });
+  });
+
+  return {
+    renderSunevent: renderSunevent,
+    setCurrentPage: setCurrentPage
+  };
+
+})(jQuery);
