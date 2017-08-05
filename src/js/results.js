810db13b9e5b7883b3f3a2b502cc0a4acbbcf97d
Michaela Blackham <Michaelablackham@gmail.com>
2017-08-05 11:48:27 -0600

    added icons for sunrise and sunset on results page

--- src/js/results.js
+++ src/js/results.js
@@ -3,12 +3,12 @@ var App = App || {};
 App.Results = (function($) {
   'use strict';
 
-  var SUNSET_TEMPLATE = '<span class="currentIcon"><img src="../src/images/@sunEvent.svg" alt="@sunEvent"/></span>';
+  var SUNSET_TEMPLATE = '<img src="../src/images/@sunEvent.svg" alt="@sunEvent"/>';
   var LOCATION_TEMPLATE = '<h2 class="heading-location"><i class="fa fa-location-arrow" aria-hidden="true"></i> @location</h2>';
   var DATE_TEMPLATE = '<h2 class="heading-date">@dow, @month @date</h2>';
   var QUALITY_TEMPLATE = '<h3 class="quality--percent">@qualityPercent%</h3>';
   var EXTRA_INFO_TEMPLATE = '<table> <tbody>' +
-    '<tr><th colspan="2">@sunIcon @sunEvent</th></tr>' +
+    '<tr><th colspan="2"><div class="flex">@sunIcon @sunEvent</div></th></tr>' +
     '<tr><td>Quality</td><td>@qualityString</td></tr>' +
     '<tr><td>Time</td><td> @time</td></tr>' +
     '<tr><td>Temperature</td><td>@temp<sup>&deg;F</sup></td></tr>' +
@@ -24,6 +24,7 @@ App.Results = (function($) {
 
     function sunIcon() {
       var sunIcon = SUNSET_TEMPLATE
+        .replace('@sunEvent', state.sunType)
         .replace('@sunEvent', state.sunType);
       return sunIcon;
     }
@@ -45,8 +46,8 @@ App.Results = (function($) {
       .replace('@qualityPercent', Math.round(state.forecast.qualityPercent));
 
     var newQualityInfo = EXTRA_INFO_TEMPLATE
-      .replace('@sunIcon', sunIcon())
       .replace('@sunEvent', state.sunType)
+      .replace('@sunIcon', sunIcon())
       .replace('@temp', App.ConvertTemp.fahrenheit())
       .replace('@qualityString', state.forecast.qualityString)
       .replace('@time', App.ConvertTime.recommendedTime());
